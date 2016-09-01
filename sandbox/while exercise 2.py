d167bcf5f0e4b920e2c128b1b1a26bcf459bb57b
Dehan Jaco Lamprecht <dehan.lamprecht@gmail.com>
2016-09-01 14:53:57 +0200

    while and for loops
    Exercises for while and for loops

---
+++ sandbox/while exercise 2.py
@@ -0,0 +1,5 @@
+count = 10
+print("Hello!")
+while count > 0:
+    print(count)
+    count -= 2
---
+++ sandbox/while exercise 3.py
@@ -0,0 +1,7 @@
+#end = 6
+#print(0.5 * end * (end + 1))
+total = 0
+while end > 0:
+    total += end
+    end -= 1
+print(total)
